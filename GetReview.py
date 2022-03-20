47a06afe5fd0a9d7c08de8307124ffd09af21b9c
Ngoc Nhu Nguyen <nguyennhungoc146@gmail.com>
2022-03-20 18:05:41 +0700

    remove Emoji

--- GetReview.py
+++ GetReview.py
@@ -1,7 +1,30 @@
 import urllib.request,sys,time
 from bs4 import BeautifulSoup
-import requests, json
+import requests, json, re
 import pandas as pd
+
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
 
 listHotel = [1370955, 5056732]
 url = "https://www.agoda.com/api/cronos/property/review/ReviewComments"
@@ -57,6 +80,7 @@ for hotelId in listHotel:
             commentText = commentText.replace(". .", ".")
             commentText = commentText.replace(". -", ".")
             commentText = commentText.replace(",","^")
+            commentText = deEmojify(commentText)
             print("Review :" + commentText)
             frame.append([rating,commentText])
             f.write(rating + "," + commentText + "\n")
